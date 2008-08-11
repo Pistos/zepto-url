dc2b4e4797a248a849dd1a8cf16e1f940789d467
Pistos <gitsomegrace.5.pistos@geoshell.com>
2008-08-11 13:33:20 -0400

    More changes taken from manveru's work.

--- start.rb
+++ start.rb
@@ -15,14 +15,14 @@ class MainController < Ramaze::Controller
     if not File.exist?( MAP_DIR )
       FileUtils.mkdir MAP_DIR
     end
+    @mutex = Mutex.new
   end
   
   # Redirect using zepto id, or show home page.
   def index( id = nil )
     if id
-      id.gsub!( /[^#{ID_CHARS}]/, '' )
-      id.gsub!( %r{[./]}, '' )
-      path = zepto_path( id )
+      index = id_to_index( id )
+      path = zepto_path( index )
       if File.exists?( path ) and File.file?( path )
         uri = File.read( path ).strip
         if not uri.empty?
@@ -38,17 +38,15 @@ class MainController < Ramaze::Controller
     @zepto_uri_only = zepto_uri_only || request[ 'zepto_uri_only' ]
     
     # Generate unique ID for the URI.
-    index = Dir[ "#{MAP_DIR }/*" ].size
-    r = 0
-    a = []
-    while index > 0
-      index, r = index.divmod ID_CHARS.size
-      a.unshift r
+    zepto_id = nil
+    next_index = nil
+    @mutex.synchronize do
+      next_index = ( Dir[ "#{MAP_DIR }/*" ].map { |filename| filename.to_i }.max || 0 ) + 1
+      zepto_id = index_to_id( next_index )
     end
-    zepto_id = a.map { |c| ID_CHARS[ c ] }.join
     
     @zepto_uri = "#{ZEPTO_URI_BASE}#{zepto_id}"
-    File.open( zepto_path( zepto_id ), 'w' ) do |f|
+    File.open( zepto_path( next_index ), 'w' ) do |f|
       f.puts uri
     end
     
@@ -58,6 +56,27 @@ class MainController < Ramaze::Controller
   def error
     "Huh? 404! Or is that 500? <a href='/'>Home</a>"
   end
+  
+  def index_to_id( index )
+    r = 0
+    zepto_id = ""
+    while index > 0
+      index, r = index.divmod ID_CHARS.size
+      zepto_id << ID_CHARS[ r ]
+    end
+    zepto_id
+  end
+  private :index_to_id
+  
+  def id_to_index( id )
+    index, r = 0, 0
+    id.scan( /./ ) do |c|
+      r = ID_CHARS.index( c )
+      index = index * ID_CHARS.size + r
+    end
+    index
+  end
+  private :id_to_index
   
   def zepto_path( id )
     "#{MAP_DIR}/#{id}"
